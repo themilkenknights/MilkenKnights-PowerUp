e506751a103483406ba1c81efb81fa89934e6158
Alexander Swerdlow <aswerdlow935@milkenschool.org>
2018-02-20 19:54:46 -0800

    Yoel Update

--- src/main/java/frc/team1836/robot/subsystems/Superstructure.java
+++ src/main/java/frc/team1836/robot/subsystems/Superstructure.java
@@ -2,8 +2,10 @@ package frc.team1836.robot.subsystems;
 
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.team1836.robot.Constants;
 import frc.team1836.robot.Constants.LOGGING;
 import frc.team1836.robot.RobotState;
+import frc.team1836.robot.util.drivers.MkLED;
 import frc.team1836.robot.util.logging.ReflectingCSVWriter;
 import frc.team1836.robot.util.loops.Loop;
 import frc.team1836.robot.util.loops.Looper;
@@ -11,120 +13,123 @@ import frc.team1836.robot.util.other.Subsystem;
 
 public class Superstructure extends Subsystem {
 
-	private final ReflectingCSVWriter<SupertructureDebugOutput> mCSVWriter;
-	PowerDistributionPanel pdp = new PowerDistributionPanel(0);
-	private SupertructureDebugOutput mDebug = new SupertructureDebugOutput();
-	private float _hue;
-	//private MkLED mkLED;
+    private final ReflectingCSVWriter<SupertructureDebugOutput> mCSVWriter;
+    PowerDistributionPanel pdp = new PowerDistributionPanel(0);
+    private SupertructureDebugOutput mDebug = new SupertructureDebugOutput();
+    private float _hue;
+    private MkLED mkLED;
 
-	public Superstructure() {
-		mCSVWriter = new ReflectingCSVWriter<>(LOGGING.SUPERSTRUCTURE_LOG_PATH,
-				SupertructureDebugOutput.class);
-	//	mkLED = new MkLED(Constants.CANIFIER_ID);
-	}
+    public Superstructure() {
+        mCSVWriter = new ReflectingCSVWriter<>(LOGGING.SUPERSTRUCTURE_LOG_PATH,
+                SupertructureDebugOutput.class);
+        mkLED = new MkLED(Constants.CANIFIER_ID);
+    }
 
-	public static Superstructure getInstance() {
-		return InstanceHolder.mInstance;
-	}
+    public static Superstructure getInstance() {
+        return InstanceHolder.mInstance;
+    }
 
-	@Override
-	public void writeToLog() {
-		mCSVWriter.write();
-	}
+    @Override
+    public void writeToLog() {
+        mCSVWriter.write();
+    }
 
-	@Override
-	public void outputToSmartDashboard() {
-		SmartDashboard.putString("System State", RobotState.mSystemState.toString());
-		updateLEDStrip();
-	}
+    @Override
+    public void outputToSmartDashboard() {
+        SmartDashboard.putString("System State", RobotState.mSystemState.toString());
+        updateLEDStrip();
+    }
 
-	@Override
-	public void stop() {
+    @Override
+    public void stop() {
 
-	}
+    }
 
-	@Override
-	public void zeroSensors() {
+    @Override
+    public void zeroSensors() {
 
-	}
+    }
 
-	@Override
-	public void checkSystem() {
-		if (pdp.getVoltage() < 10) {
-			System.out.println("FAILED - PDP VOLTAGE LOW");
-		}
-	}
+    @Override
+    public void checkSystem() {
+        if (pdp.getVoltage() < 10) {
+            System.out.println("FAILED - PDP VOLTAGE LOW");
+        }
+    }
 
-	@Override
-	public void registerEnabledLoops(Looper enabledLooper) {
-		Loop mLoop = new Loop() {
+    @Override
+    public void registerEnabledLoops(Looper enabledLooper) {
+        Loop mLoop = new Loop() {
 
-			@Override
-			public void onStart(double timestamp) {
-				synchronized (Superstructure.this) {
-					pdp.clearStickyFaults();
-				}
-			}
+            @Override
+            public void onStart(double timestamp) {
+                synchronized (Superstructure.this) {
+                    pdp.clearStickyFaults();
+                }
+            }
 
-			@Override
-			public void onLoop(double timestamp) {
-				synchronized (Superstructure.this) {
-					updateLEDStrip();
-					switch (RobotState.mSystemState) {
-						case IDLE:
-							break;
-						default:
-							break;
-					}
-					mDebug.SuperstructureState = RobotState.mSystemState.toString();
-					mDebug.PDPVoltage = 0;
-					mDebug.PDPTotalPower = 0;
-					mDebug.PDPTemp = 0;
-					mDebug.timestamp = timestamp;
-					mCSVWriter.add(mDebug);
-				}
-			}
+            @Override
+            public void onLoop(double timestamp) {
+                synchronized (Superstructure.this) {
+                    updateLEDStrip();
+                    switch (RobotState.mSystemState) {
+                        case IDLE:
+                            break;
+                        default:
+                            break;
+                    }
+                    mDebug.SuperstructureState = RobotState.mSystemState.toString();
+                    mDebug.PDPVoltage = 0;
+                    mDebug.PDPTotalPower = 0;
+                    mDebug.PDPTemp = 0;
+                    mDebug.timestamp = timestamp;
+                    mCSVWriter.add(mDebug);
+                }
+            }
 
-			@Override
-			public void onStop(double timestamp) {
-				stop();
-			}
-		};
-		enabledLooper.register(mLoop);
-	}
+            @Override
+            public void onStop(double timestamp) {
+                stop();
+            }
+        };
+        enabledLooper.register(mLoop);
+    }
 
-	private void updateLEDStrip() {
-		switch (RobotState.mMatchState) {
-			case AUTO:
-	//			mkLED.rgbSet(0, 66, 255);
-			case TELEOP:
+    private void updateLEDStrip() {
+
+        switch (RobotState.mMatchState) {
+            case AUTO:
+                //			mkLED.rgbSet(0, 66, 255);
+            case TELEOP:
+                mkLED.rgbSet(0, 66, 255);
 				/*float[] pulse1 = {4,255,0};
 				float[] pulse2 = {0,251,255};
 				mkLED.setPulse(pulse1, pulse2); */
-		//		mkLED.rgbSet(4, 255, 0);
-			case DISABLED:
-			//	mkLED.rgbSet(255, 0, 0);
-		}
+                //		mkLED.rgbSet(4, 255, 0);
+            case DISABLED:
+                mkLED.rgbSet(52, 52, 92);
+                //	mkLED.rgbSet(255, 0, 0);
+        }
 
-	}
+    }
 
-	public void setLED(float[] color) {
-		//mkLED.set_rgb(color[0], color[1], color[2]);
-	}
+    public void setLED(float[] color) {
+        //mkLED.set_rgb(color[0], color[1], color[2]);
+    }
 
 
-	public static class SupertructureDebugOutput {
+    public static class SupertructureDebugOutput {
 
-		public String SuperstructureState;
-		public double PDPVoltage;
-		public double PDPTotalPower;
-		public double PDPTemp;
-		public double timestamp;
-	}
+        public String SuperstructureState;
+        public double PDPVoltage;
+        public double PDPTotalPower;
+        public double PDPTemp;
+        public double timestamp;
+    }
 
-	private static class InstanceHolder {
+    private static class InstanceHolder {
 
-		private static final Superstructure mInstance = new Superstructure();
+        private static final Superstructure mInstance = new Superstructure();
 
-	}
+    }
 }
