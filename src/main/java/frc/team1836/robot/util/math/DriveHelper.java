201e77fb2f422d3312ac8ef7b9c48cc885ced749
Alexander Swerdlow <aswerdlow1@gmail.com>
2018-10-11 15:50:50 -0700

    Update

--- src/main/java/frc/team1836/robot/util/math/DriveHelper.java
+++ src/main/java/frc/team1836/robot/util/math/DriveHelper.java
@@ -42,7 +42,7 @@ public class DriveHelper {
             }
         }
 
-        return new DriveSignal(leftMotorSpeed, rightMotorSpeed);
+        return new DriveSignal(leftMotorSpeed/2, rightMotorSpeed/2);
     }
 
     protected static double limit(double num) {
